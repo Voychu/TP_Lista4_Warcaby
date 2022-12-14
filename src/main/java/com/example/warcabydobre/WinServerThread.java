a161d1be52e489bfbeab916620b637cae26bd725
JanP2002 <268446@student.pwr.edu.pl>
2022-12-14 17:03:05 +0100

    Adds WinServerThread
    Class servicing Server

---
+++ src/main/java/com/example/warcabydobre/WinServerThread.java
@@ -0,0 +1,38 @@
+package com.example.warcabydobre;
+
+import java.io.*;
+import java.net.*;
+
+
+public class WinServerThread {
+
+    public static void main(String[] args) {
+
+        try (ServerSocket serverSocket = new ServerSocket(4444)) {
+
+            System.out.println("Server is listening on port 4444");
+
+            while (true) {
+                Socket firstClient = serverSocket.accept();
+                System.out.println("First client connected");
+                System.out.println("Waiting for the second player");
+
+                Socket secondClient = serverSocket.accept();
+                System.out.println("Second client connected");
+
+                Game g = new Game(firstClient, secondClient);
+                Thread gTh = new Thread(g);
+                gTh.start();
+
+                // TO DO: Musi byc dokldnie dwoch klientow
+
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Server exception: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+}
+
+
