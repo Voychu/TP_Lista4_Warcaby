dab4825eb30e2e6305152e84f1cb4004f66114a1
JanP2002 <268446@student.pwr.edu.pl>
2023-01-05 14:51:35 +0100

    Removes old, unnecessary classes

--- src/main/java/com/example/warcabydobre/Piece.java
+++
@@ -1,9 +0,0 @@
-/*package com.example.warcabydobre;
-
-import javafx.scene.shape.Circle;s
-
-public abstract class Piece extends Circle{
-    public double mouseX, mouseY;
-    public double oldX, oldY;
-    public Piece(double radius){super(radius);}
-}*/
