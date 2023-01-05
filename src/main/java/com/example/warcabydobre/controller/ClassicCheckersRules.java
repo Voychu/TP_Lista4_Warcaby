1997a5a6c50bc3f558cf6e37f9e3d62a83204b0e
JanP2002 <268446@student.pwr.edu.pl>
2023-01-05 20:32:31 +0100

    Preparing GameRules class

--- src/main/java/com/example/warcabydobre/controller/ClassicCheckersRules.java
+++ src/main/java/com/example/warcabydobre/controller/ClassicCheckersRules.java
@@ -1,11 +1,19 @@
 package com.example.warcabydobre.controller;
 
+import com.example.warcabydobre.view.Move;
+
 public class ClassicCheckersRules implements GameRules{
 
 	@Override
 	public boolean isMoveValid() {
 		// TODO Auto-generated method stub
 		return false;
+	}
+
+	@Override
+	public Move tryMove(int oldX, int oldY, int newX, int newY) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
--- src/main/java/com/example/warcabydobre/controller/GameRules.java
+++ src/main/java/com/example/warcabydobre/controller/GameRules.java
@@ -1,8 +1,15 @@
 package com.example.warcabydobre.controller;
 
+import com.example.warcabydobre.view.GraphicalPiece;
+import com.example.warcabydobre.view.Move;
+
 public interface GameRules {
 	
 	boolean isMoveValid();
+	
+	public Move tryMove(int oldX, int oldY, int newX, int newY);
+	//TODO: sprawdzic waznosc ruchu. Jesli nie, to zwrocic NONE
+	//TODO: Jesli dobry, to zwrocic nowe wspolrzedne intowe oraz fakt, czy jest bicie i czy zmienia sie w damke
 	
 	
 
