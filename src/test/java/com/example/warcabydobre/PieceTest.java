49b409d135662398d8bbaf53b2efcc463a185ab5
wojtek <wojtekbaj123@gmail.com>
2023-01-06 10:27:07 +0100

    working on tests

---
+++ src/test/java/com/example/warcabydobre/PieceTest.java
@@ -0,0 +1,18 @@
+package com.example.warcabydobre;
+
+import com.example.warcabydobre.view.GraphicalPiece;
+import com.example.warcabydobre.view.PieceColor;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class PieceTest {
+
+    @Test
+    public void TestPiece() {
+        GraphicalPiece piece = new GraphicalPiece(PieceColor.WHITE,3,5);
+        assertEquals(piece.getColor(), Color.WHITE);
+    }
+}
