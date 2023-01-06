c353936f6c9e1c7c3f7d3632bcc89dce2bf4e6b3
JanP2002 <268446@student.pwr.edu.pl>
2023-01-06 12:20:36 +0100

    Corrects PieceTest JUnit Test case

--- src/test/java/com/example/warcabydobre/PieceTest.java
+++ src/test/java/com/example/warcabydobre/PieceTest.java
@@ -2,9 +2,12 @@ package com.example.warcabydobre;
 
 import com.example.warcabydobre.view.GraphicalPiece;
 import com.example.warcabydobre.view.PieceColor;
+
+
+
 import org.junit.Test;
 
-import java.awt.*;
+
 
 import static org.junit.Assert.assertEquals;
 
@@ -13,6 +16,6 @@ public class PieceTest {
     @Test
     public void TestPiece() {
         GraphicalPiece piece = new GraphicalPiece(PieceColor.WHITE,3,5);
-        assertEquals(piece.getColor(), Color.WHITE);
+        assertEquals(piece.getColor(), PieceColor.WHITE);
     }
 }
