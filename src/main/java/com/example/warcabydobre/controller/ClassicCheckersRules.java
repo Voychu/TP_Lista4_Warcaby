bc61d3cbb4252cd3f589f8a888a8f5e48e25bbaf
JanP2002 <268446@student.pwr.edu.pl>
2023-01-07 21:02:16 +0100

    Corrects tryMove method in ClassicCheckersRules

--- src/main/java/com/example/warcabydobre/controller/ClassicCheckersRules.java
+++ src/main/java/com/example/warcabydobre/controller/ClassicCheckersRules.java
@@ -6,56 +6,47 @@ import com.example.warcabydobre.model.ModelMove;
 import com.example.warcabydobre.model.MovementTypes;
 import com.example.warcabydobre.model.PieceObject;
 
-
-public class ClassicCheckersRules implements GameRules{
+public class ClassicCheckersRules implements GameRules {
 
 	private BoardModel boardModel;
-	 
-	 
-	
-	 
+
 	public ClassicCheckersRules(BoardModel boardModel) {
 		this.boardModel = boardModel;
 	}
-	
+
 	@Override
 	public boolean isMoveValid() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	//TODO: sprawdzic waznosc ruchu. Jesli nie, to zwrocic NONE
-	//TODO: Jesli dobry, to zwrocic nowe wspolrzedne intowe oraz fakt, czy jest bicie i czy zmienia sie w damke
+	// TODO: sprawdzic waznosc ruchu. Jesli nie, to zwrocic NONE
+	// TODO: Jesli dobry, to zwrocic nowe wspolrzedne intowe oraz fakt, czy jest
+	// bicie i czy zmienia sie w damke
 	@Override
-	public ModelMove tryMove(int oldX, int oldY, int newX, int newY) throws InvalidMoveException{
-		if(boardModel.isOccupied(newX, newY) || !boardModel.isBlackSquare(newX, newY))
-        {
-        return new ModelMove(MovementTypes.NONE);
-        }
-    //System.out.println(xp + ", " + yp);
-	
-	PieceObject pieceObject = boardModel.getPiecesArray()[oldX][oldY];
-	if(pieceObject == null) {
-		throw new InvalidMoveException("Na tym polu nie ma pionka");
-	}
-    if(Math.abs(newX - oldX) == 1 && newY - oldY == pieceObject.getMovementDirection())
-        return new ModelMove(MovementTypes.FORWARD);
-    else if (Math.abs(newX - oldX) == 2 && newY - oldY == pieceObject.getMovementDirection()* 2)
-        {
-            int x1 = oldX + (newX - oldX)/2;
-            int y1 = oldY + (newY - oldY)/2;
-            
-            PieceObject secondPiece = boardModel.getPiecesArray()[x1][y1];
+	public ModelMove tryMove(int oldX, int oldY, int newX, int newY) throws InvalidMoveException {
+		if (boardModel.isOccupied(newX, newY) || !boardModel.isBlackSquare(newX, newY)) {
+			return new ModelMove(MovementTypes.NONE);
+		}
+		// System.out.println(xp + ", " + yp);
 
-            if(boardModel.isOccupied(x1,y1) && secondPiece.getColor() != pieceObject.getColor())
-                {
-                return new ModelMove(MovementTypes.CAPTURE_FORWARD, secondPiece);
-                }
-        }
-    return new ModelMove(MovementTypes.NONE);
+		PieceObject pieceObject = boardModel.getPiecesArray()[oldX][oldY];
+		if (pieceObject == null) {
+			throw new InvalidMoveException("Na tym polu nie ma pionka");
+		}
+		if (Math.abs(newX - oldX) == 1 && newY - oldY == pieceObject.getMovementDirection())
+			return new ModelMove(MovementTypes.FORWARD);
+		else if (Math.abs(newX - oldX) == 2 && newY - oldY == pieceObject.getMovementDirection() * 2) {
+			int x1 = oldX + (newX - oldX) / 2;
+			int y1 = oldY + (newY - oldY) / 2;
+
+			PieceObject secondPiece = boardModel.getPiecesArray()[x1][y1];
+
+			if (boardModel.isOccupied(x1, y1) && secondPiece.getColor() != pieceObject.getColor()) {
+				return new ModelMove(MovementTypes.CAPTURE_FORWARD, secondPiece);
+			}
+		}
+		return new ModelMove(MovementTypes.NONE);
 	}
-	
-	
-	
 
 }
