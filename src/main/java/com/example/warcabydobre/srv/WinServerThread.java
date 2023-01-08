d40812fe4c1b5c9ad34355430dfc2be525b6a710
wojtek <wojtekbaj123@gmail.com>
2023-01-08 16:04:27 +0100

    Simple loop that allows only two clients running at the same time - others get NO I/O

--- src/main/java/com/example/warcabydobre/srv/WinServerThread.java
+++ src/main/java/com/example/warcabydobre/srv/WinServerThread.java
@@ -8,17 +8,20 @@ public class WinServerThread {
 
     public static void main(String[] args) {
 
+        Socket[] players = new Socket[2];
         try (ServerSocket serverSocket = new ServerSocket(4444)) {
 
             System.out.println("Server is listening on port 4444");
 
-            while (true) {
+            /**while (true) {
                 Socket firstClient = serverSocket.accept();
                 System.out.println("First client connected");
                 System.out.println("Waiting for the second player");
 
+
                 Socket secondClient = serverSocket.accept();
                 System.out.println("Second client connected");
+
 
                 Game g = new Game(firstClient, secondClient);
                 Thread gTh = new Thread(g);
@@ -26,7 +29,17 @@ public class WinServerThread {
 
                 // TODO: Musi byc dokldnie dwoch klientow
 
+            }*/
+
+            for(int i=0; i<2; i++)
+            {
+                players[i] = serverSocket.accept();
+                System.out.println("Player connected");
             }
+            System.out.println("Two players connected");
+            Game g = new Game(players[0],players[1]);
+            Thread gTh = new Thread(g);
+            gTh.start();
 
         } catch (IOException ex) {
             System.out.println("Server exception: " + ex.getMessage());
