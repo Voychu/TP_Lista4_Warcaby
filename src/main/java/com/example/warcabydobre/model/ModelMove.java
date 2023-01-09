a1e921067a47b47918b4f8ba5e25ad81cd56a624
JanP2002 <268446@student.pwr.edu.pl>
2023-01-09 16:40:05 +0100

    Corrects ModelMove class
    Adds coordinates representing fields

--- src/main/java/com/example/warcabydobre/model/ModelMove.java
+++ src/main/java/com/example/warcabydobre/model/ModelMove.java
@@ -3,6 +3,10 @@ import com.example.warcabydobre.view.GraphicalPiece;
 
 public class ModelMove {
     private MovementTypes type;
+    private int oldX;
+    private int oldY;
+    private int newX;
+    private int newY;
 
     public MovementTypes getMovementType(){
         return type;
@@ -13,11 +17,51 @@ public class ModelMove {
     public PieceObject getPieceObject(){
         return pieceObject;
     }
-    public ModelMove(MovementTypes type, PieceObject pieceObject){
+    
+//    public ModelMove(MovementTypes type, PieceObject pieceObject){
+//        this.type = type;
+//        this.pieceObject = pieceObject;
+//    }
+    public ModelMove(MovementTypes type){
+        //this(type,null);
+    	this.type=type;
+    }
+    
+    public ModelMove(MovementTypes type, PieceObject pieceObject, int oldX, int oldY, int newX, int newY){
         this.type = type;
         this.pieceObject = pieceObject;
+        this.oldX = oldX;
+        this.oldY = oldY;
+        this.newX = newX;
+        this.newY = newY;
     }
-    public ModelMove(MovementTypes type){
-        this(type,null);
+    
+    public ModelMove(MovementTypes type, int oldX, int oldY, int newX, int newY){
+        this(type,null,oldX,oldY,newX,newY);
     }
+	/**
+	 * @return the oldX
+	 */
+	public int getOldX() {
+		return oldX;
+	}
+	/**
+	 * @return the oldY
+	 */
+	public int getOldY() {
+		return oldY;
+	}
+	/**
+	 * @return the newX
+	 */
+	public int getNewX() {
+		return newX;
+	}
+	/**
+	 * @return the newY
+	 */
+	public int getNewY() {
+		return newY;
+	}
+	
 }
