355a75401d4afc44e343fc6aa7068a2c8c0f758c
wojtek <wojtekbaj123@gmail.com>
2023-01-18 22:38:01 +0100

    TryMoveForward and Capture

--- src/test/java/com/example/warcabydobre/controller/TestClassicCheckersRules.java
+++ src/test/java/com/example/warcabydobre/controller/TestClassicCheckersRules.java
@@ -17,7 +17,28 @@ public class TestClassicCheckersRules {
         ClassicCheckersRules classicCheckersRules = new ClassicCheckersRules(boardModel);
         ModelMove modelMove2 = classicCheckersRules.tryMove(0,0,1,1);
         assertEquals(modelMove.getMovementType(),modelMove2.getMovementType());
+        ModelMove modelMove3 = classicCheckersRules.tryMove(1,0,0,1);
+        assertEquals(modelMove3.getMovementType(),modelMove.getMovementType());
     }
+    @Test
+    public void TryMoveForwardTest() throws InvalidMoveException{
+        BoardModel boardModel = new BoardModel(1);
+        ClassicCheckersRules classicCheckersRules = new ClassicCheckersRules(boardModel);
+        ModelMove modelMove = new ModelMove(MovementTypes.FORWARD,1,2,0,3);
+        ModelMove modelMove2 = classicCheckersRules.tryMove(1,2,0,3);
+        assertEquals(modelMove.getMovementType(),modelMove2.getMovementType());
+    }
+  /*  @Test
+
+   public void TryCaptureTest() throws InvalidMoveException{
+        BoardModel boardModel = new BoardModel(1);
+        ClassicCheckersRules classicCheckersRules = new ClassicCheckersRules(boardModel);
+        classicCheckersRules.tryMove(1,2,2,3);
+        classicCheckersRules.tryMove(4,5,3,4);
+        ModelMove modelMove = new ModelMove(MovementTypes.SINGLE_CAPTURE,2,3,4,5);
+        ModelMove modelMove2 = classicCheckersRules.tryMove(2,3,4,5);
+        assertEquals(modelMove2.getMovementType(),modelMove.getMovementType());
+    } */
 
 
 }
