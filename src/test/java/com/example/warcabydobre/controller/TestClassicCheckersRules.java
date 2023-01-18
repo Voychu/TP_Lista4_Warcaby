a794963fc7634fcec988eaf6929e8b409d1d2b58
wojtek <wojtekbaj123@gmail.com>
2023-01-18 22:49:37 +0100

    attempt at Capture_test

--- src/test/java/com/example/warcabydobre/controller/TestClassicCheckersRules.java
+++ src/test/java/com/example/warcabydobre/controller/TestClassicCheckersRules.java
@@ -4,7 +4,14 @@ import com.example.warcabydobre.model.BoardModel;
 import com.example.warcabydobre.model.InvalidMoveException;
 import com.example.warcabydobre.model.ModelMove;
 import com.example.warcabydobre.model.MovementTypes;
+import com.example.warcabydobre.srvhandler.ServerHandler;
+import com.example.warcabydobre.view.GraphicalPiece;
+import com.example.warcabydobre.view.Square;
+import javafx.scene.Group;
+import javafx.scene.control.Label;
 import org.junit.Test;
+
+import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -28,11 +35,19 @@ public class TestClassicCheckersRules {
         ModelMove modelMove2 = classicCheckersRules.tryMove(1,2,0,3);
         assertEquals(modelMove.getMovementType(),modelMove2.getMovementType());
     }
-  /*  @Test
-
-   public void TryCaptureTest() throws InvalidMoveException{
+  /* @Test
+    public void TryCaptureTest() throws InvalidMoveException, IOException {
         BoardModel boardModel = new BoardModel(1);
         ClassicCheckersRules classicCheckersRules = new ClassicCheckersRules(boardModel);
+        GraphicalPiece graphicalPiecesArray[][] = new GraphicalPiece[8][8];
+        Square[][] board = new Square[8][8];
+        String adress = "fdsa";
+        int port = 4444;
+        ServerHandler serverHandler = new ServerHandler(adress, port);
+        Label turnLabel = new Label();
+        Group piecesGroup = new Group();
+        GameController gameController = new GameController(boardModel,graphicalPiecesArray,board,
+               serverHandler, turnLabel , piecesGroup);
         classicCheckersRules.tryMove(1,2,2,3);
         classicCheckersRules.tryMove(4,5,3,4);
         ModelMove modelMove = new ModelMove(MovementTypes.SINGLE_CAPTURE,2,3,4,5);
