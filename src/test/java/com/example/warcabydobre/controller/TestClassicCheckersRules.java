6b0547a3fd5b194eed1448a90e4c51f3f7d99c58
wojtek <wojtekbaj123@gmail.com>
2023-01-18 21:26:50 +0100

    TryMoveNoneTest - in progress

---
+++ src/test/java/com/example/warcabydobre/controller/TestClassicCheckersRules.java
@@ -0,0 +1,23 @@
+package com.example.warcabydobre.controller;
+
+import com.example.warcabydobre.model.BoardModel;
+import com.example.warcabydobre.model.InvalidMoveException;
+import com.example.warcabydobre.model.ModelMove;
+import com.example.warcabydobre.model.MovementTypes;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestClassicCheckersRules {
+
+    @Test
+    public void TryMoveNoneTest() throws InvalidMoveException {
+        BoardModel boardModel = new BoardModel(1);
+        ModelMove modelMove = new ModelMove(MovementTypes.NONE,0,0,1,1);
+        ClassicCheckersRules classicCheckersRules = new ClassicCheckersRules(boardModel);
+        assertEquals(classicCheckersRules.tryMove(0,0,1,1),modelMove.getMovementType());
+        assertEquals(classicCheckersRules.tryMove(0,0,1,1),MovementTypes.NONE);
+    }
+
+
+}
