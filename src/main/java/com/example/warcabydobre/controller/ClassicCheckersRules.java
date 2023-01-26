1bf5dd1cf3df2b84be87dea0c747e0a1f4744b37
JanP2002 <268446@student.pwr.edu.pl>
2023-01-26 19:57:43 +0100

    Adds obligation of capture to not Queen pieces
    It should be implemented on queens

--- src/main/java/com/example/warcabydobre/controller/ClassicCheckersRules.java
+++ src/main/java/com/example/warcabydobre/controller/ClassicCheckersRules.java
@@ -1,5 +1,7 @@
 package com.example.warcabydobre.controller;
 
+import java.util.LinkedList;
+
 import com.example.warcabydobre.model.BoardModel;
 import com.example.warcabydobre.model.InvalidMoveException;
 import com.example.warcabydobre.model.ModelMove;
@@ -10,21 +12,21 @@ import com.example.warcabydobre.utils.Config;
 import com.example.warcabydobre.view.PieceColor;
 
 /**
- * The class responsible for checking moves 
- * according to classic checkers game's
+ * The class responsible for checking moves according to classic checkers game's
  * rules.
  * 
  */
 public class ClassicCheckersRules implements GameRules {
 
-	
-	
-	
-	
-	
+	/** The Constant numCols number of columns in board */
+	private static final int numCols = Config.CLASSICAL_CHECKERS_BOARD_WIDTH;
+
+	/** The Constant numRows number of rows in board */
+	private static final int numRows = Config.CLASSICAL_CHECKERS_BOARD_HEIGHT;
+
+
 	/**
-	 * Model of the game that stores current pieces' 
-	 * positions and properties.
+	 * Model of the game that stores current pieces' positions and properties.
 	 */
 	private BoardModel boardModel;
 
@@ -36,90 +38,206 @@ public class ClassicCheckersRules implements GameRules {
 	public ClassicCheckersRules(BoardModel boardModel) {
 		this.boardModel = boardModel;
 	}
-	
-	
+
 	/**
-	 * Method returning color of pieces
-	 * of passed player
-	 * @param player number of player in
-	 * the game
+	 * Method returning color of pieces of passed player
+	 * 
+	 * @param player number of player in the game
 	 * @return color of player's pieces
-	 * @throws InvalidPlayerException the exception 
-	 * thrown when player number is incorrect
+	 * @throws InvalidPlayerException the exception thrown when player number is
+	 *                                incorrect
 	 */
 	@Override
 	public PieceColor getPieceColor(int player) throws InvalidPlayerException {
-		if(player == Config.FIRST) {
+		if (player == Config.FIRST) {
 			return PieceColor.WHITE;
 		}
-		if(player == Config.SECOND) {
+		if (player == Config.SECOND) {
 			return PieceColor.BLACK;
-		}
-		else throw new InvalidPlayerException("Nieprawidlowy numer gracza");
+		} else
+			throw new InvalidPlayerException("Nieprawidlowy numer gracza");
 	}
 
-	
-
 	/**
-	 * Method checking if queen can
-	 * move to passed field according
-	 * to classic checkers' rules.
+	 * Method checking if queen can move to passed field according to classic
+	 * checkers' rules.
+	 * 
 	 * @param oldX x coordinate of initial position of the move
 	 * @param oldY y coordinate of initial position of the move
 	 * @param newX x coordinate of final position of the move
 	 * @param newY y coordinate of final position of the move
-	 * @return true, if queen can move to the given field
-	 * on the board
+	 * @return true, if queen can move to the given field on the board
 	 */
 	private boolean isQueenDiagonalMove(int oldX, int oldY, int newX, int newY) {
-		if(!(Math.abs(newX - oldX) == Math.abs(newY - oldY))) {
+		if (!(Math.abs(newX - oldX) == Math.abs(newY - oldY))) {
 			return false;
 		}
 		try {
 			int numberOfWhite = boardModel.countPiecesBetween(oldX, oldY, newX, newY, PieceColor.WHITE);
 			int numberOfBlack = boardModel.countPiecesBetween(oldX, oldY, newX, newY, PieceColor.BLACK);
-			if(numberOfWhite + numberOfBlack <= 1) {
+			if (numberOfWhite + numberOfBlack <= 1) {
 				return true;
-			}
-			else {
+			} else {
 				return false;
 			}
-		} 
-		catch (NotDiagonalException e) {
+		} catch (NotDiagonalException e) {
 			return false;
 		}
 	}
-	
+
+	// TODO: implmentcja metody
 	/**
-	 * Method checking whether player
-	 * playing with pieces of passed color
-	 * can move
-	 * @param color color of pieces of
-	 * checked player
+	 * Method checking whether piece can move
+	 * 
+	 * @param piece passed piece
+	 * @return true, if piece can move
+	 */
+	public boolean canPieceMove(PieceObject piece) {
+		int pieceX = piece.getX();
+		int pieceY = piece.getY();
+		int possibleX;
+		int possibleY;
+		if (piece.isQueen()) {
+
+		} else {
+			int stepY = piece.getMovementDirection();
+			possibleY = pieceY + stepY;
+			if (!boardModel.isOccupied(pieceX + 1, possibleY)) {
+				return true;
+			}
+			if (!boardModel.isOccupied(pieceX - 1, possibleY)) {
+				return true;
+			}
+			// if((!boardModel.isOccupied(pieceX - 2, pieceY - 2)) )
+
+		}
+		return true;
+	}
+
+	/**
+	 * Method checking whether player playing with pieces of passed color can move
+	 * 
+	 * @param color color of pieces of checked player
 	 * @return true, if player can move
 	 */
 	@Override
 	public boolean canPlayerMove(PieceColor color) {
 		int numCols = Config.CLASSICAL_CHECKERS_BOARD_WIDTH;
 		int numRows = Config.CLASSICAL_CHECKERS_BOARD_HEIGHT;
-		PieceObject[][] piecesArray = boardModel.getPiecesArray(); 
-		for(int y = 0; y < numRows; y++) {
-			for(int x = 0; x < numCols; x++) {
-				if(boardModel.isOccupied(x, y)) {
-					PieceObject currentPiece = 
-							piecesArray[x][y];
-					if(currentPiece.getColor() == color) {
+		PieceObject[][] piecesArray = boardModel.getPiecesArray();
+		for (int y = 0; y < numRows; y++) {
+			for (int x = 0; x < numCols; x++) {
+				if (boardModel.isOccupied(x, y)) {
+					PieceObject currentPiece = piecesArray[x][y];
+					if (currentPiece.getColor() == color) {
 						return true;
 					}
 				}
 			}
 		}
 		return false;
-		//TODO:Poprawa metody, aby uwzgledniala ona, ze
-		//gracz moze byc zablokowany
+		// TODO:Poprawa metody, aby uwzgledniala ona, ze
+		// gracz moze byc zablokowany
+
+	}
+	
+	private boolean isCaptureMove(int oldX, int oldY, int newX, int newY) {
+		if(newX < 0 || newX >= numCols || newY < 0 || newY >= numRows) {
+			return false;
+		}
+		if(oldX < 0 || oldX >= numCols || oldY < 0 || oldY >= numRows) {
+			return false;
+		}
+		PieceObject pieceObject = boardModel.getPiecesArray()[oldX][oldY];
+		if (pieceObject == null) {
+			return false;
+		}
+		if (boardModel.isOccupied(newX, newY) || !boardModel.isBlackSquare(newX, newY)) {
+			return false;
+		}
+		if (Math.abs(newX - oldX) != 2 || Math.abs(newY - oldY) != 2) {
+			return false;
+		}
 		
+		int x1 = oldX + (newX - oldX) / 2;
+		int y1 = oldY + (newY - oldY) / 2;
+		PieceObject secondPiece = boardModel.getPiecesArray()[x1][y1];
+		if (boardModel.isOccupied(x1, y1) && secondPiece.getColor() != pieceObject.getColor()) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+
+	/**
+	 * Method returning optimal capture
+	 * 
+	 * @param color of the player's pieces
+	 * @return optimal capture if exists
+	 */
+	private ModelMove getOptimalCapture(PieceColor color) {
+		LinkedList<PieceObject> piecesList = new LinkedList<>();
+		PieceObject[][] piecesArray = boardModel.getPiecesArray();
+		for (int y = 0; y < numRows; y++) {
+			for (int x = 0; x < numCols; x++) {
+				PieceObject currPiece = piecesArray[x][y];
+				if(boardModel.isOccupied(x, y) && currPiece.getColor() == color) {
+					piecesList.add(currPiece);
+				}
+					
+			}
+		}
+		for(PieceObject piece : piecesList) {
+			int oldX = piece.getX();
+			int oldY = piece.getY();
+			int newX = oldX - 2;
+			int newY = oldY - 2;
+			if(isCaptureMove(oldX, oldY, newX, newY)) {
+				int x1 = oldX + (newX - oldX) / 2;
+				int y1 = oldY + (newY - oldY) / 2;
+				PieceObject secondPiece = boardModel.getPiecesArray()[x1][y1];
+				return new ModelMove(MovementTypes.SINGLE_CAPTURE, secondPiece, oldX, oldY, newX, newY);
+			}
+			newX = oldX - 2;
+			newY = oldY + 2;
+			if(isCaptureMove(oldX, oldY, newX, newY)) {
+				int x1 = oldX + (newX - oldX) / 2;
+				int y1 = oldY + (newY - oldY) / 2;
+				PieceObject secondPiece = boardModel.getPiecesArray()[x1][y1];
+				return new ModelMove(MovementTypes.SINGLE_CAPTURE, secondPiece, oldX, oldY, newX, newY);
+			}
+			newX = oldX + 2;
+			newY = oldY - 2;
+			if(isCaptureMove(oldX, oldY, newX, newY)) {
+				int x1 = oldX + (newX - oldX) / 2;
+				int y1 = oldY + (newY - oldY) / 2;
+				PieceObject secondPiece = boardModel.getPiecesArray()[x1][y1];
+				return new ModelMove(MovementTypes.SINGLE_CAPTURE, secondPiece, oldX, oldY, newX, newY);
+			}
+			newX = oldX + 2;
+			newY = oldY + 2;
+			if(isCaptureMove(oldX, oldY, newX, newY)) {
+				int x1 = oldX + (newX - oldX) / 2;
+				int y1 = oldY + (newY - oldY) / 2;
+				PieceObject secondPiece = boardModel.getPiecesArray()[x1][y1];
+				return new ModelMove(MovementTypes.SINGLE_CAPTURE, secondPiece, oldX, oldY, newX, newY);
+			}
+		}
+		return new ModelMove(MovementTypes.NONE);
+
+	}
+	
+	
+	private boolean canPlayerCapture(PieceColor color) {
+		ModelMove optimalCapture = getOptimalCapture(color);
+		if(optimalCapture.getMovementType() == MovementTypes.SINGLE_CAPTURE) {
+			return true;
+		}
+		return false;
 	}
 
+	
 	
 	
 	/**
@@ -146,25 +264,27 @@ public class ClassicCheckersRules implements GameRules {
 		PieceColor oppositeColor = pieceObject.getOppositeColor();
 		PieceColor myColor = pieceObject.getColor();
 		
+		ModelMove optimalCapture = getOptimalCapture(myColor);
+
 		if (pieceObject.isQueen() && isQueenDiagonalMove(oldX, oldY, newX, newY)) {
 			try {
-				int numberOfMyPieces = 
-						boardModel.countPiecesBetween(oldX, oldY, newX, newY, myColor);
-				int numberOfEnemysPieces = 
-						boardModel.countPiecesBetween(oldX, oldY, newX, newY, oppositeColor);
-				if(numberOfEnemysPieces == 1 && numberOfMyPieces == 0) {
+				int numberOfMyPieces = boardModel.countPiecesBetween(oldX, oldY, newX, newY, myColor);
+				int numberOfEnemysPieces = boardModel.countPiecesBetween(oldX, oldY, newX, newY, oppositeColor);
+				if (numberOfEnemysPieces == 1 && numberOfMyPieces == 0) {
 					return new ModelMove(MovementTypes.QUEEN_CAPTURE, oldX, oldY, newX, newY);
 				}
-				if(numberOfEnemysPieces == 0 && numberOfMyPieces == 0) {
+				if (numberOfEnemysPieces == 0 && numberOfMyPieces == 0
+						&& !canPlayerCapture(myColor)) {
 					return new ModelMove(MovementTypes.QUEEN_DIAGONAL, oldX, oldY, newX, newY);
 				}
 			} catch (NotDiagonalException e) {
 				System.out.println(0);
 			}
-			
+
 		}
 
-		if (Math.abs(newX - oldX) == 1 && newY - oldY == pieceObject.getMovementDirection())
+		if (Math.abs(newX - oldX) == 1 && newY - oldY == pieceObject.getMovementDirection()
+				&& !canPlayerCapture(myColor))
 			return new ModelMove(MovementTypes.FORWARD, oldX, oldY, newX, newY);
 		else if (Math.abs(newX - oldX) == 2 && Math.abs(newY - oldY) == 2) {
 			int x1 = oldX + (newX - oldX) / 2;
@@ -179,5 +299,4 @@ public class ClassicCheckersRules implements GameRules {
 		return new ModelMove(MovementTypes.NONE);
 	}
 
-	
 }
